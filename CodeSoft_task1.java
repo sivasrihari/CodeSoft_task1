965193c62ecf47dd7a3c1ca40c8c4d17fd42406d
sivasrihari <119554131+sivasrihari@users.noreply.github.com>
2024-03-13 19:58:26 +0530

    Add files via upload

---
+++ CodeSoft_task1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class CodeSoft_task1 
+{
+    public int generate(int min,int max)
+    {
+        return (int)(Math.random()*(max-min+1)+min);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        CodeSoft_task1 rg = new CodeSoft_task1();
+        int totalAttempts = 0;
+        int win = 0;
+
+        while (true) {
+            System.out.println("Enter the maximum number");
+            int max = sc.nextInt();
+            System.out.println("Enter the minimum number");
+            int min = sc.nextInt();
+            sc.nextLine();
+
+            int cnum = rg.generate(max, min);
+            int attempts = 0;
+
+            while (true) {
+                System.out.println("Guess a number between "+max+" and "+min);
+                int gnum = sc.nextInt();
+                attempts++;
+
+                if (gnum > cnum) {
+                    System.out.println("Its Greater");
+                } else if (gnum < cnum){
+                    System.out.println("Its Lower");
+                }else{
+                    System.out.println("Correct Guess");
+                    win++;
+                    break;
+                }
+            }
+            totalAttempts += attempts;
+            System.out.println("Attempts = " + attempts);
+            System.out.println("Wins = " + win);
+
+            double winrate = (double) win / totalAttempts*100;
+            System.out.printf("Your winrate is %.2f%%\n",winrate);
+
+            System.out.println("Do you want to play again (yes/no)");
+            String playAgain = sc.next();
+            if(!playAgain.equalsIgnoreCase("yes")){
+                sc.close();
+                System.exit(0);
+            }
+            sc.nextLine();
+        }
+    }
+    }
+    
